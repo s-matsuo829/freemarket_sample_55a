9271aaa86a27b1442c75584b04b6a96043ea4cef
s-matsu829 <fakefur829@gmail.com>
2019-07-24 15:20:23 +0900

    add_basic_Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == '55_mercari_a' && password == '55a'
+    end
+  end
 end
