e0115e324f290378d85ead081efdc5e91df3e5a2
NanaTake <son_magomago@yahoo.co.jp>
2019-07-24 16:55:09 +0900

    Update user.rb
    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  with_options presence: true do
+    validates :nickname
+    validates :first_name
+    validates :last_name
+    validates :first_kana
+    validates :last_kana
+    validates :birthday
+  end
 end
