aa0d39a713f23d505e99e572a6a7e103e1fb01b2
s-matsu829 <fakefur829@gmail.com>
2019-07-24 15:03:56 +0900

    exit s3

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,8 +51,8 @@ end
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["access_key_id"],
-  AWS_SECRET_ACCESS_KEY: ENV["secret_access_key"]
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
 
 
