70c846d970d14f1cec68f3fa47ce5e0f153ad2bf
NanaTake <son_magomago@yahoo.co.jp>
2019-07-24 17:34:11 +0900

    Check nickname
    nicknameのバリデーションチェック
    クリア

--- README.md
+++ README.md
@@ -44,7 +44,7 @@ Things you may want to cover:
 - has_many :items
 - has_one :card
 - has_many :likes, dependent: :destroy
-- has_many :buyer_tradings, class_name: 'Trading', :foreign_key => 'buyer_id'
+- has_many :buyer_tradings, class_name: 'Trading', foreign_key: 'buyer_id'
 
 - validates :email, presence: true, uniqueness: true
 - with_options presence: true do
---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+
+  factory :user do
+    nickname              {"abe"}
+    email                 {"kkk@gmail.com"}
+    password              {"00000000"}
+    password_confirmation {"00000000"}
+    first_name            {"名前"}
+    last_name             {"名字"}
+    first_kana            {"ナマエ"}
+    last_kana             {"ミョウジ"}
+    birthday              {"2019-07-24"}
+  end
+
+end
