78e7e4574d56c43b18dc80e2d54e8fb1b5243c00
s-matsu829 <fakefur829@gmail.com>
2019-07-25 14:50:28 +0900

    edit_hello

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,13 +8,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # if Rails.env.development?
-    # storage :file
-  # elsif Rails.env.test?
-    # storage :file
-  # else
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
     storage :fog
-  # end
+  end
   # storage :file
   # storage :fog
 
