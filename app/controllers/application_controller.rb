7560a789116c0f800ad16bd41c31ecb87c1caca1
s-matsu829 <fakefur829@gmail.com>
2019-07-25 15:39:42 +0900

    edit_basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
   private
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
-    end
+  end
   # http_basic_authenticate_with name: "aaaa", password: "1111"
 end
