f27560e3cceaa646d41f745cd6d6b68aedb1a314
NanaTake <son_magomago@yahoo.co.jp>
2019-07-26 17:38:44 +0900

    routing/controller
    この先のマイページ作成に必要だと思われるルーティングとコントローラーを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/index'
+  get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, :controllers => {
+    :sessions      => "users/sessions",
+    :registrations => "users/registrations",
+    :passwords     => "users/passwords",
+    # :omniauth_callbacks => "users/omniauth_callbacks" 
+  }
+  resources :users, :only => [:index, :show]
+
 end
