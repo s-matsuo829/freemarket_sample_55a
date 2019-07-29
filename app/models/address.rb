269d204908d7790bd1390c116ed130dc6d050d2d
NanaTake <son_magomago@yahoo.co.jp>
2019-07-29 15:37:49 +0900

    Update scss
    scss完成

--- app/models/address.rb
+++ app/models/address.rb
@@ -15,54 +15,6 @@ class Address < ApplicationRecord
     uniqueness: true,
     format: { with: reg_intger_10or11_characters }
   
-  enum prefecture: %w(-- 北海道 青森県 岩手県)
-    # <option >--</option>
-    # <option >北海道</option>
-    # <option >青森県</option>
-    # <option >岩手県</option>
-    # <option >宮城県</option>
-    # <option >秋田県</option>
-    # <option >山形県</option>
-    # <option >福島県</option>
-    # <option >東京都</option>
-    # <option >神奈川県</option>
-    # <option >埼玉県</option>
-    # <option >千葉県</option>
-    # <option >茨城県</option>
-    # <option >栃木県</option>
-    # <option >群馬県</option>
-    # <option >山梨県</option>
-    # <option >新潟県</option>
-    # <option >長野県</option>
-    # <option >富山県</option>
-    # <option >石川県</option>
-    # <option >福井県</option>
-    # <option >愛知県</option>
-    # <option >岐阜県</option>
-    # <option >静岡県</option>
-    # <option >三重県</option>
-    # <option  selected>大阪府</option>
-    # <option >兵庫県</option>
-    # <option >京都府</option>
-    # <option >滋賀県</option>
-    # <option >奈良県</option>
-    # <option >和歌山県</option>
-    # <option >鳥取県</option>
-    # <option >島根県</option>
-    # <option >岡山県</option>
-    # <option >広島県</option>
-    # <option >山口県</option>
-    # <option >徳島県</option>
-    # <option >香川県</option>
-    # <option >愛媛県</option>
-    # <option >高知県</option>
-    # <option >福岡県</option>
-    # <option >佐賀県</option>
-    # <option >長崎県</option>
-    # <option >熊本県</option>
-    # <option >大分県</option>
-    # <option >宮崎県</option>
-    # <option >鹿児島県</option>
-    # <option >沖縄県</option>
-    # </select>
+  enum prefecture: %w(-- 北海道 青森県 岩手県 宮城県 秋田県 山形県 福島県 東京都 神奈川県 埼玉県 千葉県 茨城県 栃木県 群馬県 山梨県 新潟県 長野県 富山県 石川県 福井県 愛知県 岐阜県 静岡県 三重県 大阪府 兵庫県 京都府 滋賀県 和歌山県 鳥取県 島根県 岡山県 広島県 山口県 徳島県 香川県 愛媛県 高知県 福岡県 佐賀県 長崎県 熊本県 大分県 宮崎県 鹿児島県 沖縄県)
+  
 end
