35373f00d7cd17d1ee1c659352c8c96f2bbaec72
NanaTake <son_magomago@yahoo.co.jp>
2019-08-05 18:08:24 +0900

    Update haml
    ロゴのリンクを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  devise :validatable, password_length: 6..128
 
   has_one :address
 
