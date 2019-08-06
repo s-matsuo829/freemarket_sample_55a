25ee30d453780dc36c039bb452e856d35abacb31
NanaTake <son_magomago@yahoo.co.jp>
2019-08-06 18:04:04 +0900

    Complete！
    メールアドレスでの新規登録で、全ての登録画面へ移動出来る様になりました。
    住所の登録も出来る様に修正しました。
    やったぜ。

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,12 +1,25 @@
 class AddressesController < ApplicationController
 
   def new
+    @address = Address.new
   end
 
   def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to new_user_card_path(current_user.id), notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def address_params
+    params.permit(:postalcode, :prefecture, :city, :number, :building, :phone_number).merge(user_id: current_user.id)
   end
 
 end
