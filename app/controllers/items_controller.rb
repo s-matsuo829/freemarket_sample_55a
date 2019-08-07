17537e2272f3de44ac91845d08e1e16a13cb34ce
NanaTake <son_magomago@yahoo.co.jp>
2019-08-07 15:41:09 +0900

    Update edit.html.haml
    画面が表示される状態にする
    ついでにcontrollerも数値をint型に受け取れるように改造手術

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,9 +12,33 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.user_id == current_user.id
+      @item.update(
+        image: item_params[:image],
+        name: item_params[:name],
+        description: item_params[:description],
+        item_status: item_params[:item_status].to_i,
+        payment: item_params[:payment].to_i,
+        delivery_type: item_params[:delivery_type].to_i,
+        delivery_region: item_params[:delivery_region].to_i,
+        delivery_days: item_params[:delivery_days].to_i,
+        price: item_params[:price]
+      )
+      redirect_to item_path(@item.id)
+    else
+      render item_path(item.id)
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :item_status, :payment, :delivery_type, :delivery_region, :delivery_days, :price).merge(user_id: current_user.id)
   end
 
 end
