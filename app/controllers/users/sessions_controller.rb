87778d0aa99a80f3863f1f690e24bb3588e61b1c
NanaTake <son_magomago@yahoo.co.jp>
2019-08-08 12:17:30 +0900

    Update sessions_controller.rb
    一旦、ログイン後のリダイレクト先指定を外してdeviseの基本の挙動にお任せしてみました

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -26,10 +26,10 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # サインイン後のリダイレクト先をマイページへ
-  def after_sign_in_path_for(resource)
-    # flash[:notice] = "ログインに成功しました" #　 <-任意で
-    user_path(current_user.id)  #　指定したいパスに変更
-  end
+  # def after_sign_in_path_for(resource)
+  #   # flash[:notice] = "ログインに成功しました" #　 <-任意で
+  #   user_path(current_user.id)  #　指定したいパスに変更
+  # end
 
   # サインアウト後のリダイレクト先をトップページへ
   def after_sign_out_path_for(resource)
