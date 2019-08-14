3d96eba612e8343e2674553db9cd056ea6240c68
s-matsu829 <fakefur829@gmail.com>
2019-08-14 17:54:24 +0900

    check_user_signed_in?
    ・未ログイン時のエラー防止
    ・current_userのアイテムが新着順になっていなかったのを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,8 +7,8 @@ class ItemsController < ApplicationController
   def index
     @items = Item.order("created_at DESC")
     @items_limit_4 = @items.limit(4)
-    @items_user = current_user.items.limit(4)
-    @items_other = @items.where.not(user_id: current_user.id).limit(4)
+    @items_user = current_user.items.limit(4).order("created_at DESC") if user_signed_in?
+    @items_other = @items.where.not(user_id: current_user.id).limit(4) if user_signed_in?
   end
 
 
