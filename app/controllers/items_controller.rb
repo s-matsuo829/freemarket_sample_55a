b72fdd86099887e6e3549f76c9524ef57af0879d
s-matsu829 <fakefur829@gmail.com>
2019-08-15 11:15:38 +0900

    add_allitems_status_check
    全体の新着アイテムのindexの表示にも出品中のみの指定を追加した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,7 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.order("created_at DESC")
-    @items_limit_4 = @items.limit(4)
+    @items_limit_4 = @items.left_joins(:trading).where(tradings: {status: "出品中"}).limit(4)
     if user_signed_in?
       @items_user = current_user.items.limit(4).order("created_at DESC")
       @items_other = @items.left_joins(:trading).where(tradings: {status: "出品中"}).where.not(user_id: current_user.id).limit(4)
