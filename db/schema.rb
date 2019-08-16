4845d8ce71a17c9ea802a0c05b81ece3a04eab16
ksk-yoshiura <mrtambourineman8358@gmail.com>
2019-08-16 11:21:43 +0900

    editted 8/16
    deleted unnecesarry files and changed hash rockets into colon.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_08_12_072635) do
-
+ActiveRecord::Schema.define(version: 2019_08_12_110328) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postalcode", null: false
