82a40e2d3259b1660eb896aef9fc55fd8b3aea72
ksk-yoshiura <mrtambourineman8358@gmail.com>
2019-08-22 16:10:15 +0900

    editted 8/22
    Before merging into the master.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -64,6 +64,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def create
     if params[:user][:password].nil?
+      binding.pry
       @user = User.create(user_via_sns_params)
       sns = SnsCredential.create(user_id: @user.id,uid: params[:user][:uid], provider: params[:user][:provider])
     else 
