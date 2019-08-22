d7867146e8a9ca650189de11ae7d5f8bae5c05ae
s-matsu829 <fakefur829@gmail.com>
2019-08-22 17:41:10 +0900

    edit_selection
    余分な記述をコメントアウト

--- db/schema.rb
+++ db/schema.rb
@@ -59,14 +59,6 @@ ActiveRecord::Schema.define(version: 2019_08_22_035851) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "uid"
-    t.string "provider"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "tradings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "item_id", null: false
     t.integer "status", null: false
